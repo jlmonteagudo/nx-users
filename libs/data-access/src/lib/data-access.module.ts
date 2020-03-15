d6f3ddd5c5400f3164d38f64c28dc1fbac647631
Jose Luis Monteagudo <jl_monteagudo@yahoo.es>
2020-03-15 11:42:10 +0100

    Updated to Angular 9 and Nx 9

--- libs/data-access/src/lib/data-access.module.ts
+++ libs/data-access/src/lib/data-access.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { DataAccessOptions } from './data-access.options';
@@ -12,11 +12,18 @@ import { DataAccessOptions } from './data-access.options';
 })
 export class DataAccessModule {
 
-  static forRoot(options?: DataAccessOptions) {
-    return <ModuleWithProviders> {
+  constructor (@Optional() @SkipSelf() parentModule?: DataAccessModule) {
+    if (parentModule) {
+      throw new Error(
+        'DataAccessModule is already loaded. Import it in the AppModule only');
+    }
+  }  
+
+  static forRoot(options: DataAccessOptions): ModuleWithProviders {
+    return {
       ngModule: DataAccessModule,
       providers: [
-        { provide: DataAccessOptions, useValue: options || {} },
+        { provide: DataAccessOptions, useValue: options },
       ],
     };
   }
